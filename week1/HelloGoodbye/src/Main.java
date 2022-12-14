a75ad8be3811ea731f6f26b04273721a7975e551
Aleksandr Goncharov <cooperjkr@ya.ru>
2022-12-14 12:19:59 +0500

    week1: Обработка аргументов командной строки в HelloGoodbye

--- week1/HelloGoodbye/src/Main.java
+++ week1/HelloGoodbye/src/Main.java
@@ -1,5 +1,8 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.printf("Hello %s and %s\n", args[0], args[1]);
+        System.out.printf("Goodbye %s and %s\n", args[1], args[0]);
     }
 }
