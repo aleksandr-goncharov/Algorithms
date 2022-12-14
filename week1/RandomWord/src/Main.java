df1d9931b4808a4a57ea9ad2460489f2aa5a0875
Aleksandr Goncharov <cooperjkr@ya.ru>
2022-12-14 13:06:56 +0500

    week1: Вывод рандомного слова в RandomWord

--- week1/RandomWord/src/Main.java
+++ week1/RandomWord/src/Main.java
@@ -1,7 +1,22 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world!");
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Main {
+    public static void main(String[] args) {
+        var championWord = (String)null;
+        var wordsCount = 0;
+
+        while (!StdIn.isEmpty()) {
+            var word = StdIn.readString();
+
+            wordsCount++;
+
+            if (StdRandom.bernoulli(1.0/wordsCount)) {
+                championWord = word;
+            }
+        }
+
+        StdOut.println(championWord);
     }
 }
